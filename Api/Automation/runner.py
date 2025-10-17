5425e6af5ccb0be18548ec5bb8eededb39140ca4
pritam guha <pguha3828@gmail.com>
2025-10-17 11:15:06 +0530

    addedtest_login_missing_fields

--- Api/Automation/runner.py
+++ Api/Automation/runner.py
@@ -9,14 +9,17 @@ if __name__ == "__main__":
 
     choice = input("Enter your choice (1/2/3): ").strip()
 
+    html_report = "./report.html"  # report file path
+
     if choice == "1":
-        exit_code = pytest.main(["-v", "./Api/Automation/Tests/test_login.py"])
+        exit_code = pytest.main(["-v", "./Api/Automation/Tests/test_login.py", f"--html={html_report}"])
     elif choice == "2":
-        exit_code = pytest.main(["-v", "./Api/Automation/Tests/test_category.py"])
+        exit_code = pytest.main(["-v", "./Api/Automation/Tests/test_category.py", f"--html={html_report}"])
     elif choice == "3":
-        exit_code = pytest.main(["-v", "./Api/Automation/Tests/"])
+        exit_code = pytest.main(["-v", "./Api/Automation/Tests/", f"--html={html_report}"])
     else:
         print("Invalid choice")
         sys.exit(1)
 
+    print(f"\nHTML report generated at: {html_report}")
     sys.exit(exit_code)
